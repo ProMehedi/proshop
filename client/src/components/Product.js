ecfb8a9986b8fa8c3fac833be21dad8d9cee62d9
promehedi <mmhasan563@gmail.com>
2021-02-11 02:16:07 +0600

    Serving products from backend

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -6,7 +6,7 @@ import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <>
-      <Card className='my-2'>
+      <Card className='mb-4'>
         <Link to={`/product/${product._id}`}>
           <Card.Img src={product.image} variant='top' />
         </Link>
---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import products from './data/products.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Server is ready!')
+})
+
+app.get('/api/products', (req, res) => {
+  res.send(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((prodId) => prodId._id === req.params.id)
+  if (product) {
+    res.send(product)
+  } else {
+    res.status(404).send({ message: 'Product not found!' })
+  }
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
