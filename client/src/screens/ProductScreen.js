399a84a13d5b84f4e448d869dd573c5adc21a363
promehedi <mmhasan563@gmail.com>
2021-02-10 20:41:03 +0600

    Product Details Screen

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,10 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Col, Image, ListGroup, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const [qty, setQty] = useState(1)
+
+  const qtyHandler = (e) => {
+    if (qty < 0) {
+      return false
+    } else if (qty < 50) {
+      setQty(e.target.value)
+    }
+  }
+
+  const product = products.find((p) => p._id === match.params.id)
+
   return (
-    <div>
-      <h1>Single Product</h1>
-    </div>
+    <>
+      <Link className='btn btn-dark mb-3' to='/'>
+        <i className='fa fa-double-angle-left'></i> Go Back to home
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+
+        <Col md={6}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item className='px-0'>
+              <h3>{product.name}</h3>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} Reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item className='px-0'>
+              <h4>
+                <strong>Price: </strong>${product.price}
+              </h4>
+            </ListGroup.Item>
+            <ListGroup.Item className='px-0'>
+              <strong>Discription: </strong>
+              {product.description}
+            </ListGroup.Item>
+            <ListGroup.Item className='px-0'>
+              <strong>Status:</strong>{' '}
+              {product.countInStock === 0 ? (
+                <span className='text-danger'>
+                  Out of Stock{' '}
+                  <span className='text-dark'>
+                    ({product.countInStock} Available)
+                  </span>
+                </span>
+              ) : product.countInStock <= 5 ? (
+                <span className='text-warning'>
+                  Low Stock{' '}
+                  <span className='text-dark'>
+                    ({product.countInStock} Available)
+                  </span>
+                </span>
+              ) : (
+                <span className='text-success'>
+                  In Stock{' '}
+                  <span className='text-dark'>
+                    ({product.countInStock} Available)
+                  </span>
+                </span>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item className='px-0'>
+              <div className='d-flex align-items-center'>
+                <strong>QTY:</strong>
+                <div className='qtyGroup px-3'>
+                  <input
+                    className='quantity form-control'
+                    type='number'
+                    min='1'
+                    max={product.countInStock}
+                    value={qty}
+                    onChange={qtyHandler}
+                  />
+                </div>
+                <Button
+                  variant='primary'
+                  onClick={() => console.log('test')}
+                  disabled={product.countInStock === 0 ? true : false}
+                >
+                  <i className='fa fa-shopping-cart'></i> Add To Cart
+                </Button>
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
   )
 }
 
