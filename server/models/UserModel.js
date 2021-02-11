090341c2380763402bc1c1719303dbcfea3e3bac
promehedi <mmhasan563@gmail.com>
2021-02-12 03:16:36 +0600

    User Auth Endpoint

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
@@ -9,6 +10,10 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 )
+
+userSchema.methods.matchPassword = async function (enteredPass) {
+  return await bcrypt.compare(enteredPass, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
