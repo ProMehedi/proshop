a0b1a61a4e4d0d5c8d5ac29b903410155e8afdb0
promehedi <mmhasan563@gmail.com>
2021-02-12 04:36:48 +0600

    User login Reducers & Actions

--- client/src/store.js
+++ client/src/store.js
@@ -5,19 +5,25 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers'
+import { userLoginReducers } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducers,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
