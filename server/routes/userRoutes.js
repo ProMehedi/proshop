5f3158597da7bf9082293f70e381ea856777a51f
promehedi <mmhasan563@gmail.com>
2021-02-12 04:19:07 +0600

    Reg users & Encrypt pass

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import {
+  authUser,
+  getUserProfile,
+  regiterUser,
+} from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.route('/').post(regiterUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
